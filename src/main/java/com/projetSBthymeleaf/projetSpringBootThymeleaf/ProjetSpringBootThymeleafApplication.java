d32cf5e3ab25d2dbfb2333450e7ed21f630f7678
JulieStevenin <140088946+JulieStevenin@users.noreply.github.com>
2023-09-11 09:49:31 +0200

    premiÃer commit

---
+++ src/main/java/com/projetSBthymeleaf/projetSpringBootThymeleaf/ProjetSpringBootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.projetSBthymeleaf.projetSpringBootThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSpringBootThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetSBthymeleaf/projetSpringBootThymeleaf/ProjetSpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetSBthymeleaf.projetSpringBootThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringBootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
